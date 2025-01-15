b6858e3c533726cabec4842be07f18516df4b861
NurShafiqka <eqkashafiqka@gmail.com>
2025-01-15 23:28:51 +0800

    Update lectprofile_edit_action.php

--- lectprofile_edit_action.php
+++ lectprofile_edit_action.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include_once 'config.php';
+include 'config.php';
 
 $lecturerId = $_SESSION['lecturer_id'];
 
@@ -93,12 +93,8 @@ if (isset($_POST['submit'])) {
                     echo "Sorry, there was an error uploading your profile picture.<br>";
                     echo '<a href="javascript:history.back()">Back</a>';
                 }
-            } else {
-                echo '<a href="javascript:history.back()">Back</a>';
-            }
-        } else {
-            echo '<a href="javascript:history.back()">Back</a>';
-        }
+            } 
+        } 
     }
     // If there is no image to be uploaded
     else {
